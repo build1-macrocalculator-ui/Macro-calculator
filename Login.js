4d9620d82575e00211c9efdd68033db1f9ff35b2
cwern40 <chris.wernli@hotmail.com>
2019-05-25 11:11:28 -0600

    I added the column-content class to the bottom section. I also added media queries to the bottom section. I added a feature to the login window to make the page dim while the login window is open

--- Login.js
+++ Login.js
@@ -1,9 +1,11 @@
 const display = document.querySelectorAll('.button');
 const login = document.querySelector('.login-screen');
 const exit = document.querySelector('.close-button');
+const container = document.querySelector('.home-container');
 
 let toggle = function() {
-    return login.classList.toggle('login-hidden');
+    container.classList.toggle('toggle');
+    login.classList.toggle('login-hidden');
 };
 
 exit.addEventListener('click', () => {
@@ -13,5 +15,19 @@ exit.addEventListener('click', () => {
 display.forEach(function(element) {
     element.addEventListener('click', () => {
     toggle();
-})
+    })
+});
+
+const feature = document.querySelectorAll('.column-content');
+
+feature.forEach(function(element) {
+    element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = 'lightgray';
+    })
+});
+
+feature.forEach(function(element) {
+    element.addEventListener('mouseleave', () => {
+        element.style.backgroundColor = 'white';
+    })
 });
